1f03e4d237dc8239ef486887a6e5311e84dbf313
Maedemdi <maedemajidi.381@gmail.com>
2025-07-04 17:14:11 +0330

    Added an APIView name HelloAPI

--- profiles_app/views.py
+++ profiles_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloAPI(APIView):
+    """ Testing the APIView with a dummy list """
+    def get(self, request, format=None):
+        shopping_list = ['salt', 'pepper', 'pepper spray', 'door knob']
+        return Response({'message': 'Hello!', 'shopping_list':shopping_list})
